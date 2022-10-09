8f842a41530d1a5e74ce75841765e000cabc283f
Roy_Tsui <hdxzj@qq.com>
2022-10-09 15:36:43 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,12 @@ npm run build
 git add -A
 git commit -m 'deploy'
 
-git push origin master
+# git push origin master
 
 # 如果你要部署在 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:j172993563/poker-game.git main:gh-pages
+git push -f git@github.com:j172993563/poker-game.git main
 
 cd -
